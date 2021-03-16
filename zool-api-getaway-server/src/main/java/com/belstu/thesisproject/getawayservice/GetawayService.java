ab1ee83d285c6c20b175854be6c5502ab1fea129
ilyaaleks <ilya.aleks.2011@gmail.com>
2021-03-16 22:30:12 +0300

    Merge remote-tracking branch 'origin/BE-TP-2-imrove-formatting'
    # Conflicts:
    #	medical-records-service/src/main/java/com/belstu/thesisproject/mrs/domain/ProblemType.java
    #	psycho-user-service/src/main/java/com/belstu/thesisproject/psychouserservice/domain/Doctor.java
    #	psycho-user-service/src/main/java/com/belstu/thesisproject/psychouserservice/domain/Patient.java

---
+++ zool-api-getaway-server/src/main/java/com/belstu/thesisproject/getawayservice/GetawayService.java
@@ -0,0 +1,15 @@
+package com.belstu.thesisproject.getawayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class GetawayService {
+  public static void main(String[] args) {
+    SpringApplication.run(GetawayService.class, args);
+  }
+}
